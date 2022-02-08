9ba72eceb0266026c63a8beccab01d2ed1a919e6
Jared Heeringa <jaredheeringa@icloud.com>
2022-02-07 20:57:36 -0600

    Added class tester for BlackJack

---
+++ src/BlackJackTester.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class BlackJackTester {
+    public static void main(String[] args) {
+        BlackJack game = new BlackJack();
+
+        game.playGame();
+    }
+}
